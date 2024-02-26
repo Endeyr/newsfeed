92936e4b470da68ef71a3357714b1dc3c56866e0
Endeyr <aaron.thompson42594@gmail.com>
2024-02-26 09:23:01 -0500

    rm export from queryClient

--- app/tanstack/page.tsx
+++ app/tanstack/page.tsx
@@ -3,7 +3,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import GetData from '.'
 
-export const queryClient = new QueryClient()
+const queryClient = new QueryClient()
 export default function Page() {
 	return (
 		<QueryClientProvider client={queryClient}>
