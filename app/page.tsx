a484a35436269d2fccd6f1b1160eb9cb89ae53e3
Endeyr <aaron.thompson42594@gmail.com>
2024-02-28 08:27:47 -0500

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Container from './components/container'
+import Hero from './components/hero'
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between  ">
-			Home
-		</main>
+		<Container>
+			<Hero />
+		</Container>
 	)
 }
